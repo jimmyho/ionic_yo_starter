c4da379661f38a844d91b4e2bcec4a8fb97e4f09
Jimmy Ho <jho@jimmyho.com>
2015-03-24 22:12:20 +0000

    calling Login

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,7 +2,7 @@
 
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, Auth) {
   // Form data for the login modal
   $scope.loginData = {};
 
@@ -26,12 +26,16 @@ angular.module('starter.controllers', [])
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
     console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
+    Auth.login($scope.loginData.username, $scope.loginData.password)
+      .then(function(){
+        console.log('success')
+      })
+      .catch(function(err){
+        console.error(err)
+      })
+      .finally(function(){
+        $scope.closeLogin();
+      })
   };
 })
 
